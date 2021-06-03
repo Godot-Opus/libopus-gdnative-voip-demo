07027b64794e5841e9b83833cf0cece9f2592004
Wavesonics <adamwbrown@gmail.com>
2021-06-02 17:48:07 -0700

    Added osx support

--- voip.gd
+++ voip.gd
@@ -7,6 +7,12 @@ var record
 var recording = false
 var time_elapsed = 0
 
+export(NodePath) var opusEncoderPath
+onready var opusEncoder := get_node(opusEncoderPath)
+
+export(NodePath) var opusDecoderPath
+onready var opusDecoder := get_node(opusDecoderPath)
+
 func _ready():
 	mic = AudioServer.get_bus_effect(AudioServer.get_bus_index("Record"), 0)
 
@@ -14,7 +20,7 @@ remote func _play(id, opusPackets, format, mix_rate, stereo):
 	get_node("/root/Control/Log").text += "received audio from player with id: %s\n" % id
 
 	# Decode the incoming packets into raw PCM data
-	var pcmData = $OpusDecoder.decode(opusPackets)
+	var pcmData = opusDecoder.decode(opusPackets)
 
 	var audioStream = AudioStreamSample.new()
 	audioStream.data = pcmData
@@ -33,7 +39,7 @@ func _process(delta: float) -> void:
 				var pcmData = record.get_data()
 				
 				# Encode the raw PCM data to a stream of Opus Packets
-				var opusEncoded = $OpusEncoder.encode(pcmData)
+				var opusEncoded = opusEncoder.encode(pcmData)
 				
 				rpc_unreliable("_play",get_tree().get_network_unique_id(), opusEncoded, record.get_format(), record.get_mix_rate(), record.is_stereo())
 				get_node("/root/Control/Log").text += "send recording of size %s\n" % record.get_data().size()
